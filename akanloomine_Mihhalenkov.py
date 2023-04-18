0480d8f7fc48a50d0eb907fa51615461c2a974f5
ArtemiMihhalenkov <120027616+Sufed@users.noreply.github.com>
2023-04-18 20:15:03 +0300

    Update akanloomine_Mihhalenkov.py

--- akanloomine_Mihhalenkov.py
+++ akanloomine_Mihhalenkov.py
@@ -14,12 +14,17 @@ pygame.draw.rect(ekraani_pind,(139, 69, 19),jalg)
 ristkülik=pygame.Rect(150,200,110,130)
 pygame.draw.rect(ekraani_pind,(0, 128, 0),ristkülik)
 
-
 jalg=pygame.Rect(490,200,30,200)
 pygame.draw.rect(ekraani_pind,(139, 69, 19),jalg)
 
 ristkülik=pygame.Rect(450,200,110,130)
 pygame.draw.rect(ekraani_pind,(0, 128, 0),ristkülik)
+
+ristkülik=pygame.Rect(300,10,320,130)
+pygame.draw.ellipse(ekraani_pind,(255,255,255),ristkülik)
+
+lil=pygame.Rect(215,22,70,70)
+pygame.draw.ellipse(ekraani_pind, (250,250,0), lil)
 
 pilt=pygame.image.load("among2.png")
 ekraani_pind.blit(pilt,(330,290))

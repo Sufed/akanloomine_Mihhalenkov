5303ed74b63d3c547052638751928c0a0a91ba9e
ArtemiMihhalenkov <120027616+Sufed@users.noreply.github.com>
2023-04-18 13:41:26 +0300

    Update 2osa.py

--- 2osa.py
+++ 2osa.py
@@ -21,31 +21,16 @@ pygame.draw.ellipse(ekraani_pind, (255,165,0), lil)
 lil=pygame.Rect(123,58,65,65)
 pygame.draw.ellipse(ekraani_pind, (250,250,250), lil)
 
-ristkülik=pygame.Rect(80,85,50,7)                            
-pygame.draw.rect(ekraani_pind,(250,250,250),ristkülik)
-
-ristkülik=pygame.Rect(180,85,50,7)                            
-pygame.draw.rect(ekraani_pind,(250,250,250),ristkülik)
-
 lil=pygame.Rect(152,70,7,7)
 pygame.draw.ellipse(ekraani_pind, (0,0,0), lil)
 
 lil=pygame.Rect(152,90,7,7)
 pygame.draw.ellipse(ekraani_pind, (0,0,0), lil)
 
-ristkülik=pygame.Rect(80,50,7,120)                            
-pygame.draw.rect(ekraani_pind,(205,133,63),ristkülik)
-
 lil=pygame.Rect(113,120,85,85)
 pygame.draw.ellipse(ekraani_pind, (250,250,250), lil)
 
 
-lopp_x=120
-lopp_y=0
-for i in range(10):
-    pygame.draw.line(ekraani_pind,"#a17c38",(83,60),(lopp_x,lopp_y),3 )
-    lopp_x-=8
-    lopp_y+=2
 
 pygame.display.flip()
 

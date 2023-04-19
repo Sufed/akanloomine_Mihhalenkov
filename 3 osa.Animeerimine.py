94633757554933fadd350add05d4a550d927a0e4
ArtemiMihhalenkov <120027616+Sufed@users.noreply.github.com>
2023-04-19 12:25:42 +0300

    Update 3 osa.Animeerimine.py

--- 3 osa.Animeerimine.py
+++ 3 osa.Animeerimine.py
@@ -1,49 +1,95 @@
-import pygame,sys
+import pygame
+import sys
+import random
+
 pygame.init()
-#värvid
-kollane=[255,255,10]
-punane=[255,0,0]
-hall=[255,200,200]
-roosa=[255,150,255]
-roheline=[100,255,100]
 
-#ekraani suurus
-X=640
-Y=480
-ekraan=pygame.display.set_mode([X,Y])
-pygame.display.set_caption("Animatsion")
-ekraan.fill(roheline)
-kell=pygame.time.Clock()
-mesilane=pygame.image.load("among2.png")
-posX=X-mesilane.get_rect().width
-posY=Y-mesilane.get_rect().height
-lõpp=False
-samm=2
-while not lõpp:
-    kell.tick(60)
-    events=pygame.event.get()
-    for i in pygame.event.get():
-        if i.type==pygame.QUIT():
-            sys.exit()
-    ekraan.blit(mesilane,(posX,posY))
+def Liikumine_vastupaeva():
+    global posX, posY, samm
+    if posX <= 0:
+        posX = 0
+        posY += samm
+    if posY <= 0:
+        posY = 0
+        posX -= samm
+    if posX >= X - mesilane.get_rect().width:
+        posX = X - mesilane.get_rect().width
+        posY -= samm
+    if posY >= Y - mesilane.get_rect().height:
+        posY = Y - mesilane.get_rect().height
+        posX += samm
 
+def Liikumine_paripaeva():
+    global posX, posY, samm
     if posX <= 0:
         posX = 0
         posY -= samm
-
     if posY <= 0:
         posY = 0
-        posX += samm + 2
-
-
+        posX += samm
     if posX >= X - mesilane.get_rect().width:
         posX = X - mesilane.get_rect().width
         posY += samm
-
     if posY >= Y - mesilane.get_rect().height:
         posY = Y - mesilane.get_rect().height
-        posX -= samm + 2
+        posX -= samm
+
+def Vasakule_ja_paremale():
+    global posX, posY, samm
+    posX -= samm
+    if posX > X - mesilane.get_rect().width or posX < 0: 
+        samm = -samm
+
+def Ules_ja_alla():
+    global posX, posY, samm
+    posY -= samm
+    if posY > Y - mesilane.get_rect().width or posY < 0: 
+        samm = -samm
+
+# Варианты цветов
+kollane = [255, 255, 10]
+punane = [255, 0, 0]
+hall = [255, 200, 200]
+roosa = [255, 150, 255]
+roheline = [100, 255, 100]
+
+# Размеры экрана
+X = 640
+Y = 480
+
+ekraan = pygame.display.set_mode([X, Y])
+pygame.display.set_caption("Animatsion")
+ekraan.fill(roheline)
+
+kell = pygame.time.Clock()
+mesilane = pygame.image.load("among2.png")
+posX = X - mesilane.get_rect().width
+posY = Y - mesilane.get_rect().height
+lopp = False
+samm = 2
+suund = random.randint(-1,2)
+
+while not lopp:
+    kell.tick(60)
+    events = pygame.event.get()
+    for i in pygame.event.get():
+        sys.exit()
+
+    ekraan.blit(mesilane, (posX, posY))
+
+    if suund == 1:
+        Liikumine_vastupaeva()
+
+    if suund == -1:
+        Liikumine_paripaeva()
+
+    if suund == 0:
+        Vasakule_ja_paremale()
+
+    if suund == 2:
+        Ules_ja_alla()
 
     pygame.display.flip()
     ekraan.fill(roheline)
+
 pygame.quit()

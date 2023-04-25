4e6615e5e188fbc462da3d29b7d5116596e2a9f4
ArtemiMihhalenkov <120027616+Sufed@users.noreply.github.com>
2023-04-25 14:36:11 +0300

    Update 5.osa.py

--- 5.osa.py
+++ 5.osa.py
@@ -1,92 +1,66 @@
-import pygame
+#import pygame
 #import sys
 #import random
-#import pygame, random
 
-
-
-
-##sounds = ['snd1.mp3', 'snd2.mp3', 'snd3.mp3', 'snd4.mp3', 'snd5.mp3']
-##pygame.mixer.music.load('music/'+random.choice(sounds))
-##pygame.mixer.music.play()
-
-##pygame.mixer.music.set_volume(0.2)
-
-##hit_sound = pygame.mixer.Sound('music/Hit.wav')
-##pygame.mixer.Sound.play(hit_sound)
 #pygame.init()
 
 #X = 640
 #Y = 480
-##SCREEN_WIDTH = 640
-##SCREEN_HEIGHT = 480
-## Цвета
-#Valge = (255, 255, 255, 255)
-#Must = (0, 0, 0, 0)
+
+#Valge = (255, 255, 255)
+#Must = (0, 0, 0)
 #Kollane = (255, 255, 0)
 
-## Размеры яблока и корзины
 #Õuna_suurus  = 20
 #Korvi_laius = 30
 #Korvi_kõrgus = 30
-##APPLE_SIZE = 20
-##BASKET_WIDTH = 30
-##BASKET_HEIGHT = 30
-## Начальные координаты яблока
+
 #Õun_X = random.randint(0, X - Õuna_suurus)
 #Õun_Y = 0
 
-## Начальные координаты корзины
 #Korvi_X = X // 2 - Korvi_laius // 2
 #Korvi_Y = Y // 1.5
 
+#Õuna_kiirus= 1
 
-#Õuna_kiirus= 1 #Скорость падения яблока 
-
-## Создание окна
 #screen = pygame.display.set_mode((X, Y))
 #pygame.display.set_caption("Õuna ja korviga mängimine")
 
-## Основной цикл игры
+#õuna_pilt = pygame.image.load("õun.png") # для яблока
+#korvi_pilt = pygame.image.load(".png") #  для корзины
+
 #clock = pygame.time.Clock()
 #score = 0
+
 #while True:
 #    for event in pygame.event.get():
 #        if event.type == pygame.QUIT:
 #            pygame.quit()
 #            sys.exit()
 
-#    # Обновление координат яблока
 #    Õun_Y += Õuna_kiirus
 
-#    # Проверка столкновения яблока с основанием
 #    if Õun_Y >= Y:
 #        score -= 1
 #        Õun_X = random.randint(0, X - Õuna_suurus)
 #        Õun_Y = 0
 
-#    # Проверка столкновения яблока с корзиной
 #    if Õun_Y + Õuna_suurus >= Korvi_Y and Õun_X + Õuna_suurus >= Korvi_X and Õun_X <= Korvi_X + Korvi_laius:
 #        score += 1
 #        Õun_X = random.randint(0, X - Õuna_suurus)
 #        Õun_Y  = 0
 
-#    # Обновление координат корзины
 #    keys = pygame.key.get_pressed()
 #    if keys[pygame.K_LEFT] and Korvi_X > 0:
 #        Korvi_X -= 5
 #    if keys[pygame.K_RIGHT] and Korvi_X < X - Korvi_laius:
 #        Korvi_X += 5
 
-#    # Очистка экрана
 #    screen.fill(Valge)
 
-#    # Отрисовка яблока
-#    pygame.draw.rect(screen, Kollane (Õun_X, Õun_Y, Õuna_suurus, Õuna_suurus))
+#    pygame.draw.rect(screen, Kollane, (Õun_X, Õun_Y, Õuna_suurus, Õuna_suurus))
 
-#    # Отрисовка корзины
 #    pygame.draw.rect(screen, Must, (Korvi_X, Korvi_Y, Korvi_laius, Korvi_kõrgus))
-
 
 #    font = pygame.font.Font(None, 36)
 #    score_text = font.render("Счет: {}".format(score), True, Must)
@@ -95,7 +69,6 @@ import pygame
 #    pygame.display.flip()
 
 #    clock.tick(60)
-#    break
 
 
 
@@ -113,6 +86,17 @@ import pygame
 
 
 import pygame, random
+import pygame, random
+ 
+#sounds = ['snd1.mp3', 'snd2.mp3', 'snd3.mp3', 'snd4.mp3', 'snd5.mp3']
+#pygame.mixer.music.load('music/'+random.choice(sounds))
+#pygame.mixer.music.play()
+
+#pygame.mixer.music.set_volume(0.2)
+
+#hit_sound = pygame.mixer.Sound('music/Hit.wav')
+#pygame.mixer.Sound.play(hit_sound)
+
 pygame.init()
 
 red = [255, 0, 0]

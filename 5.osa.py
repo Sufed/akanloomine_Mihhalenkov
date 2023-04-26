4c6f7e41cc6b03fa4925fd4fd453df4ab0e77f30
ArtemiMihhalenkov <120027616+Sufed@users.noreply.github.com>
2023-04-26 12:09:57 +0300

    Update 5.osa.py

--- 5.osa.py
+++ 5.osa.py
@@ -23,11 +23,22 @@ Korvi_Y = Y // 1.5
 
 Õuna_kiirus = 5 #Скорость падения яблок.
 
+enemies=[]
+
 screen = pygame.display.set_mode((X, Y))
 pygame.display.set_caption("Õuna ja korviga mängimine")
 
 õuna_pilt = pygame.image.load("õun.png")  # для яблока
 korvi_pilt = pygame.image.load("among2.png")  # для корзины
+
+#player = pygame.Rect(X, Y, 120, 120)
+#playerImage = pygame.image.load("among2.png")
+#playerImage = pygame.transform.scale(playerImage, [player.width, player.height])
+
+#for i in range(5):
+#    enemies.append(pygame.Rect(random.randint(0, X - 100), random.randint(0,Y - 100), 60, 73))
+#enemyImage = pygame.image.load("õun.png")
+#enemyImage = pygame.transform.scale(enemyImage, [enemies[0].width, enemies[0].height])
 
 clock = pygame.time.Clock()
 score = 0
@@ -38,9 +49,9 @@ while not gameover:
         if event.type == pygame.QUIT:
             gameover=True
         elif pygame.key.get_pressed()[pygame.K_a]:
-            Korvi_X-=5
+            Korvi_X-=10
         elif pygame.key.get_pressed()[pygame.K_d]:
-            Korvi_X+=5
+            Korvi_X+=10
         #elif event.type==pygame.KEYDOWN:
         #    if event.key==pygame.K_LEFT:
         #        Korvi_X-=5
@@ -49,14 +60,22 @@ while not gameover:
 
     Õun_Y += Õuna_kiirus
 
+    #for enemy in enemies[:]:
+    #    if player.colliderect(enemy):
+    #        enemies.remove(enemy)
+    #        score += 1
+    #        Õun_X = random.randint(0, X - Õuna_suurus)
+    #        Õun_Y = 0
+
     if Õun_Y >= Y:
+        score -= 1
         Õun_X = random.randint(0, X - Õuna_suurus)
         Õun_Y = 0
 
-    #if Õun_Y + Õuna_suurus >= Korvi_Y and Õun_X + Õuna_suurus >= Korvi_X and Õun_X <= Korvi_X + Korvi_laius:
-    #    score += 1
-    #    Õun_X = random.randint(0, X - Õuna_suurus)
-    #    Õun_Y = 0
+    if Õun_Y + Õuna_suurus >= Korvi_Y and Õun_X + Õuna_suurus >= Korvi_X and Õun_X <= Korvi_X + Korvi_laius:
+        score += 1
+        Õun_X = random.randint(0, X - Õuna_suurus)
+        Õun_Y = 0
 
 
     screen.fill(Valge)
@@ -65,7 +84,8 @@ while not gameover:
     #    if player.colliderect(enemy):
     #        enemies.remove(enemy)
     #        score += 1
-
+    #        Õun_X = random.randint(0, X - Õuna_suurus)
+    #        Õun_Y = 0
     screen.blit(õuna_pilt, (Õun_X, Õun_Y)) # Отображение изображения яблока на экране
     screen.blit(korvi_pilt, (Korvi_X, Korvi_Y)) # Отображение изображения корзины на экране
 
